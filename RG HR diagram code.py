fed1375c213cf8da46bb4427059ffd1aa8ac31d2
ryan-dot-g <115864216+ryan-dot-g@users.noreply.github.com>
2023-03-02 09:50:24 +1000

    updates
    push before tute

--- RG HR diagram code.py
+++ RG HR diagram code.py
@@ -1,13 +1,12 @@
 # module imports
-import numpy as np # for maths 
-import matplotlib # for plotting 
+import numpy as np 
+import matplotlib 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import warnings
-warnings.filterwarnings('ignore')
+import warnings; warnings.filterwarnings('ignore')
 import os 
-import pandas as pd # pandas is a popular library in industry for manipulating large data tables
-
+import pandas as pd 
+ddir = 'C:\\Users\\rgray\\OneDrive\\ryan\\Uni\\2023 sem 1\\PHYS3080\\Assignments\\DL-Proj\\DATA\\' 
 
 # define default plot settings
 mpl.style.use('seaborn-colorblind') # colourblind-friendly colour scheme
@@ -17,7 +16,6 @@ matplotlib.rcParams['font.size']=16              #10
 matplotlib.rcParams['savefig.dpi']= 300             #72 
 
 # set data directory and read data
-ddir = 'C:\\Users\\rgray\\OneDrive\\ryan\\Uni\\2023 sem 1\\PHYS3080\\Assignments\\DL-Proj\\DATA\\' 
 camera = 'Top'
 stars = pd.read_csv(ddir + camera + '\\Star_Data.csv') 
 print(stars.keys()) # this tells us what column names we have
@@ -52,36 +50,50 @@ m0, m1, m2 = (np.log10(galaxy.BlueF),
               np.log10(galaxy.GreenF), 
               np.log10(galaxy.RedF) ) 
 colour = m2-m0
-s = plt.scatter(colour,m1)
+plt.scatter(colour,m1)
 plt.ylabel('Log Flux 1')
 plt.xlabel('Log Flux 2 - Log Flux 0')
 plt.title("HR diagram of galaxy")
 plt.show()
 
-# output galaxy parallaxes
-print('Parallaxes (arcsec): mean %.3f, sd %.3f' % (np.mean(galaxy.Parallax),np.std(galaxy.Parallax)))
-
 '''                 END GALAXY SECTION          '''
 
-'''             START DISTANCE ESTIMATION SECTION       '''
+'''             STAR DISTANCE CALIBRATION SECTION       '''
 
 import glob
 all_stars = glob.glob(ddir + '*/Star_Data.csv') # all star data
+parallaxCutoff = 0.01 # minimum parallax to be a valid star for distance calibration
 
-for i, catalog in enumerate(all_stars):
-    this = pd.read_csv(catalog)
-    thispar = this.Parallax
-    thism0, thism1, thism2 = (np.log10(this.BlueF),
-                              np.log10(this.GreenF),
-                              np.log10(this.RedF))
-    thiscolour = thism2 - thism0
-    dist = 1/thispar
-    abs_mag = thism1 + 2 * np.log10(dist)
-    mm = thispar > 0.01 
-    plt.scatter(thiscolour[mm], abs_mag[mm], color = 'C1')
-
-plt.xlabel('Log Flux 2 - Log Flux 0')
-plt.ylabel('Log Flux 1')
-plt.title("Stars with high parallax")
+allStarsDf = pd.concat( pd.read_csv(catalog) for catalog in all_stars ) # super dataframe containing all stars
+goodStars = allStarsDf[allStarsDf.Parallax > parallaxCutoff] # calibrate with star if sufficient parallax
+goodStars["m0"], goodStars["m1"], goodStars["m2"] = (np.log10(goodStars.BlueF),
+                                                     np.log10(goodStars.GreenF),
+                                                     np.log10(goodStars.RedF)) # different color fluxes
+goodStars["colour"] = goodStars.m2 - goodStars.m0
+goodStars["dist"] = 1/goodStars.Parallax # distance = 1/parallax where parallax in arcseconds
+goodStars["abs_mag"] = goodStars.m1 + 2 * np.log10(goodStars.dist) # absolute magnitude using log scale
+plt.scatter( goodStars.colour, goodStars.abs_mag, 
+             color = 'C1')
+plt.xlabel('Colour')
+plt.ylabel('Absolute magnitude')
+plt.title("Absolute-magnitude calibrated stars")
 plt.show()
-        
+
+'''         END STAR DISTANCE CALIBRATION SECTION   '''
+
+'''         GALAXY DISTANCE MATCHING SECTION        '''
+# distance modulus to adjust (eye test) 6.8
+dm = np.mean( goodStars.abs_mag ) - np.mean( m1 )
+delta_dm = 0.1 # uncertainty (eye test)
+
+plt.scatter( goodStars.colour, goodStars.abs_mag, 
+             color = 'C1') # calibration stars
+plt.scatter( colour, m1 + dm ) # galaxy
+plt.ylabel('Log Flux 1')
+plt.xlabel('Log Flux 2 - Log Flux 0')
+plt.title("Cluster calibration")
+plt.legend(["Benchmark", "Cluster"])
+plt.show()
+
+galaxyDist = np.power( 10, np.array([dm-delta_dm, dm+delta_dm])/2 )
+print(galaxyDist)
---
+++ RG preamble.py
@@ -0,0 +1,24 @@
+# module imports
+import numpy as np 
+import matplotlib 
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import warnings; warnings.filterwarnings('ignore')
+import os 
+import pandas as pd 
+ddir = 'C:\\Users\\rgray\\OneDrive\\ryan\\Uni\\2023 sem 1\\PHYS3080\\Assignments\\DL-Proj\\DATA\\' 
+
+# define default plot settings
+mpl.style.use('seaborn-colorblind') # colourblind-friendly colour scheme
+matplotlib.rcParams['image.origin'] = 'lower'
+matplotlib.rcParams['figure.figsize']=(8.0,6.0)    #(6.0,4.0)
+matplotlib.rcParams['font.size']=16              #10 
+matplotlib.rcParams['savefig.dpi']= 300             #72 
+
+
+
+
+
+
+
+
