5b1481696718ae69b2dddc7eae9b06349bc712b6
ryan-dot-g <115864216+ryan-dot-g@users.noreply.github.com>
2023-03-15 15:13:40 +1000

    Update RG fuzzies and flashes.py

--- RG fuzzies and flashes.py
+++ RG fuzzies and flashes.py
@@ -76,6 +76,8 @@ DEFAULT = [
 
 '''                 BEGIN USER INPUT SECTION            '''
 
+plotHubble = True
+
 CAMERAS = {"Top":TOP,"Bottom":BOTTOM,"Left":LEFT,
            "Right":RIGHT,"Front":FRONT,"Back":BACK}
 
@@ -84,9 +86,13 @@ STARS = {}
 OBJ_DISTANCES = {}
 OBJ_SPEEDS = {}
 
+GALACTIC_CENTERS = {}
+
 '''                 BEGIN ACTUAL CLUSTERING SECTION            '''
 
 for camera,cameraData in CAMERAS.items():
+    
+    GALACTIC_CENTERS[camera] = {}
 
     stars = pd.read_csv(ddir + camera + '\\Star_Data.csv') 
     
@@ -125,7 +131,7 @@ for camera,cameraData in CAMERAS.items():
                                                          np.log10(goodStars.GreenF),
                                                          np.log10(goodStars.RedF)) # different color fluxes
     goodStars["colour"] = goodStars.m2 - goodStars.m0
-    goodStars["dist"] = 1/goodStars.Parallax # distance = 1/parallax where parallax in arcseconds
+    goodStars["dist"] = 1/goodStars.Parallax # distance = 1/parallax where parallax in arcseconds and d in parsec
     goodStars["abs_mag"] = goodStars.m1 + 2 * np.log10(goodStars.dist) # absolute magnitude using log scale
     
     # galaxies that are excluded from analysis because they aren't really a galaxy
@@ -166,19 +172,40 @@ for camera,cameraData in CAMERAS.items():
             medianRV = np.median(galaxy.RadialVelocity)
 
             gxy_speeds[gxy_index] = medianRV
+            
+            GALACTIC_CENTERS[camera][gxy_index] = (np.median(galaxy.X),np.median(galaxy.Y))
 
     OBJ_DISTANCES[camera] = gxy_distances
     OBJ_SPEEDS[camera] = gxy_speeds
 
     STARS[camera] = stars
+    
+
+
 
 def get_galaxy(X,Y,stars,camera):
     ''' Takes X,Y pixel location of a flash. Returns the number galaxy it belongs
-    to in the star dataframe with galaxies from that camera. Returns None if
-    (a) the flash is not contained within any 1 galaxy or
-    (b) the galaxy it is contained in is invalid in the list of invalid galaxies '''
+    to in the star dataframe with galaxies from that camera. 
     
-    return 1
+    IDEALLY Returns None if
+    (a) the flash is not contained within any 1 galaxy or
+    (b) the galaxy it is contained in is invalid in the list of invalid galaxies.
+    Currently not implemented'''
+    
+    gcenters = GALACTIC_CENTERS[camera]
+    gdistances = {i: np.linalg.norm((X-r[0],Y-r[1])) for i,r in gcenters.items()}
+    
+    closest_galaxy = [i for i,d in gdistances.items() if d==min(dist for dist in gdistances.values())][0]
+    
+    return closest_galaxy
+
+def get_obj(X,Y,objs):
+    ''' Takes X,Y pixel location of an event observation. Returns the number
+    object it belongs to in the objs dataframe with objs from that camera '''    
+    obj_distances = {i: np.linalg.norm((X-f.X,Y-f.Y)) for i,f in objs.iterrows()}
+    closest_obj = [i for i,d in obj_distances.items() if d==min(dist for dist in obj_distances.values())][0]
+    
+    return closest_obj
     
 
 '''         BEGIN FLASH CALIBRATION SECTION             '''
@@ -189,8 +216,75 @@ nf1, nf2 = flashes.loc[35], flashes.loc[46] # selecting the 2 super bright flash
 g1 = get_galaxy(nf1.X, nf1.Y, STARS[nf1.Direction], nf1.Direction) 
 g2 = get_galaxy(nf2.X, nf2.Y, STARS[nf2.Direction], nf2.Direction) 
 
-d1 = OBJ_DISTANCES[camera][g1]  # distance of near flash 1
-d2 = OBJ_DISTANCES[camera][g2] # distance of near flash 2
+d1 = OBJ_DISTANCES[nf1.Direction][g1]  # distance of near flash 1
+d2 = OBJ_DISTANCES[nf2.Direction][g2] # distance of near flash 2
 
-absPC1 = 0; # absolute photon count, scaling by distance
-absPC2 = 0; 
+# absolute photon counts, scaled by distance using inverse-square law
+absPC1 = nf1["Photon-Count"]*(d1)**2 
+absPC2 = nf2["Photon-Count"]*(d2)**2
+
+absPC = np.mean([absPC1,absPC2]) # absolute magnitude at 1 parsec
+
+# give every flash a distance using inverse square law
+flashes["Distance"] = np.sqrt(absPC/flashes["Photon-Count"])
+
+
+'''     BEGIN FUZZY CALIBRATION SECTION             '''
+FUZ_DISTANCES = {cam:{} for cam in CAMERAS.keys()}
+FUZ_RADVEL = {cam:{} for cam in CAMERAS.keys()}
+
+# method below gives every fuzzy a distance by assigning nearest flash
+# for camera in CAMERAS.keys():
+#     fuzzies = pd.read_csv(f'DATA//{camera}/Distant_Galaxy_Data.csv')
+#     FUZZIES[camera] = fuzzies
+    
+#     FUZ_DISTANCES[camera] = {}
+#     FUZ_RADVEL[camera] = {}
+#     for i,fuzzy in fuzzies.iterrows():
+#         # locate closest flash and get its distance
+#         closestFlash = get_obj(fuzzy.X,fuzzy.Y,flashes,camera) # need to fiz and index flaashes with camera first
+#         distance = flashes.loc[closestFlash].Distance
+        
+#         # assign fuzzy galaxy that distance, record rad vel
+#         FUZ_DISTANCES[camera][i] = distance
+#         FUZ_RADVEL[camera][i] = fuzzy.RadialVelocity
+
+# method below gives only the closest fuzzy to each flash a distance
+
+for i,flash in flashes.iterrows():
+    camera = flash.Direction
+    fuzzies = pd.read_csv(f'DATA//{camera}/Distant_Galaxy_Data.csv')
+    
+    closestFuzzy = get_obj(flash.X,flash.Y,fuzzies)
+    FUZ_DISTANCES[camera][closestFuzzy] = flash.Distance
+    FUZ_RADVEL[camera][closestFuzzy] = fuzzies.loc[closestFuzzy].RadialVelocity
+    
+        
+# for camera in CAMERAS.keys():
+#     Dist = [d for d in FUZ_DISTANCES[camera].values()]
+#     RV = [rv for rv in FUZ_RADVEL[camera].values()]
+#     plt.scatter(Dist,RV)
+#     plt.show()
+        
+# flash distances and radial velocities 
+fld = np.array([d for camDistances in FUZ_DISTANCES.values() for d in camDistances.values()])
+flrv = np.array([rv for camRV in FUZ_RADVEL.values() for rv in camRV.values()])
+
+fldMpc = fld/10**6 # distances in megaparsecs
+
+poly = np.polyfit(fldMpc,flrv,1)
+H0 = poly[0]
+print(f"Hubble's constant is {H0}")
+predRV = np.polyval(poly,fldMpc)
+
+if plotHubble:
+    plt.scatter(fldMpc, flrv)
+    plt.plot(fldMpc, predRV, '--')
+    plt.xlabel("Distance (Mpc)"); plt.ylabel("Radial velocity (km/s)")
+    plt.title("Distant galaxy movement to determine Hubble's constant")
+    plt.show()
+
+
+
+
+
